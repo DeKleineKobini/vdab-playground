3b7cfadb8edc22359dec14625860a84c390cf601
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2022-01-24 13:43:57 +0100

    Add additional api info

--- playground-backend/playground-service/src/main/java/world/inetum/realdolmen/playground/service/config/SpringFoxConfiguration.java
+++ playground-backend/playground-service/src/main/java/world/inetum/realdolmen/playground/service/config/SpringFoxConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import world.inetum.realdolmen.playground.service.Application;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +21,21 @@ public class SpringFoxConfiguration {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage(Application.class.getPackageName()))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Playground UI API",
+                "API for the Playground UI.",
+                null,
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
     }
 
 }
